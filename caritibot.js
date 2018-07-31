950975831a36df1e3a207e69719107b3578f51a4
Alejandro Rabadán <caritino@users.noreply.github.com>
2018-07-31 01:50:39 -0400

    Update caritibot.js
    Added the functionality to look at the tag you are looking for and send a message.

--- caritibot.js
+++ caritibot.js
@@ -4,12 +4,23 @@ const token = 'NDczNzA4MTg0NzcxNDkzODg5.DkF5cg.Xny4BmQl1rTXOg-lmWzh10BXWOM';
 
 client.login(token);
 
+
 client.on('message', message => {
 
-    if(message.content.toLocaleLowerCase() === 'hello')
-        message.channel.send(message.channel.name.name());
+    if(message.content.includes('Details: !sbux'))
+        message.channel.send('!sbux');
 
+    else if(message.content.toLocaleLowerCase() === 'hello')
+        message.channel.send('Obligame prro! ' + message.author);
         
 
 });
 
+
+/*
+client.on("guildMemberAdd", MemberAdd => {
+
+    MemberAdd.guild.channels.find("name", "sbux").send("!sbux")
+    console.log("enter"); 
+});
+*/
